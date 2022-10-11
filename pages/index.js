8fe73b8b78d7d3de9da85f45d7bd7ee8d17a99e6
MacBook Pro <azavip@MacBook-Pro-de-yhakkach.local>
2022-10-11 23:09:54 +0100

    -/\-

--- pages/index.js
+++ pages/index.js
@@ -63,7 +63,7 @@ export default function Home() {
             <h2 className={styles.how_to_play}>How to play:</h2>
             <ul className={styles.list}>
               <li> Make sure you have an account in <a href='https://codeforces.com/register' target='_blank' rel="noreferrer">codeforces.com</a></li>
-              <li> Learn How to use codeforces (Create team, submit sulotion,...)</li>
+              <li> Learn How to use codeforces (Create team, submit your solution ,...)</li>
               <li> The teams will be given problems to solve</li>
               <li> The team with the most points wins</li>
               <li> A prize will be given to the winners (three first teams)</li>
