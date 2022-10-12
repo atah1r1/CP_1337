0cbe335fc9ce3a714a28b34c36aee789d7b2ab59
MacBook Pro <azavip@MacBook-Pro-de-yhakkach.local>
2022-10-12 17:15:19 +0100

    []

--- pages/leaderboard.js
+++ pages/leaderboard.js
@@ -36,7 +36,7 @@ export default function Leaderboard() {
                             <p>Rank</p>
                         </div>
                         <div className={styles.scroll}>
-                            {leaderboard.sort((a, b) =>  b.rank - a.rank ).map((team, i) => {
+                            {leaderboard.sort((a, b) =>   a.rank - b.rank ).map((team, i) => {
                                 return (
                                     <div className={styles.leaderboard} key={i}>
                                         <img src={`https://avatars.dicebear.com/api/bottts/${team.name}.svg`} width='70px'></img>
